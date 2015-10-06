cc7f533b52d4fc91f84bc1cae4e43ec6765f9e7a
pnmougel <pnmougel@gmail.com>
2015-10-06 11:43:06 +0200

    Fixed bug in the parser and added search ooi by name

--- app/oois/Model.scala
+++ app/oois/Model.scala
@@ -21,6 +21,17 @@ object OOI {
       case id ~ name ~ description  => OOI(id, name, description)
     }
   }
+  val idWithNameParser = {
+    get[Long]("ooi_id") ~ get[String]("name") map {
+      case id ~ name => (id, name)
+    }
+  }
+
+  val ooiWithNameAndUnitParser = {
+    get[Long]("ooi.id") ~ get[String]("ooi.name") ~ get[String]("unit.name") map {
+      case id ~ name ~ unit => (id, name, unit)
+    }
+  }
   val tableName = "ooi"
 
   def findAll(): Seq[OOI] = {
@@ -29,11 +40,26 @@ object OOI {
     }
   }
 
+  def findByNames(names: Array[String]) : List[(Long, String, String)] = {
+    val namesLower = names.map(_.toLowerCase.trim).toList
+    DB.withConnection { implicit c =>
+      // SQL"SELECT ooi_id, name FROM ooi_name WHERE name_search IN ($namesLower)".as(idWithNameParser.*)
+      SQL"""
+        SELECT ooi.id, ooi.name, unit.name
+        FROM ooi, unit
+        WHERE
+          unit.id = ooi.unit_id AND
+          ooi.id IN
+            (SELECT DISTINCT(ooi_id) FROM ooi_name WHERE name_search IN ($namesLower))
+      """.as(ooiWithNameAndUnitParser.*)
+    }
+  }
+
   def findByName(name: String) : List[OOI] = {
     val nameLower = name.toLowerCase
     DB.withConnection { implicit c =>
       SQL"""
-        SELECT * FROM #$tableName
+        SELECT * FROM ooi
         WHERE id IN
           (SELECT DISTINCT(ooi_id)
           FROM ooi_name
@@ -69,7 +95,7 @@ object OOI {
         SQL"""
           INSERT INTO ooi_name
           (name, name_search, language_id, ooi_id)
-          VALUES (${name.name}, ${name.nameSearch}, ${languageId}, $ooiId)
+          VALUES (${name.name}, ${name.nameSearch}, $languageId, $ooiId)
         """.executeInsert()
       }
       ooiId
