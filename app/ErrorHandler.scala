40416888c0b5fea1e6b5dfa0656d78d15c15fbb8
pnmougel <pnmougel@gmail.com>
2015-10-06 11:45:38 +0200

    Added default error handler

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,21 @@
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+
+class ErrorHandler extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+    Future.successful(
+      Status(statusCode)("A client error occurred: " + message)
+    )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable) = {
+    System.err.println("A server error occured")
+    exception.printStackTrace()
+    Future.successful(
+      InternalServerError("A server error occurred: " + exception.getMessage)
+    )
+  }
+}
